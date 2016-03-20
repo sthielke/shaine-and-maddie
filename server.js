14afa667f3b20b05cb7444528b5733626bc4276a
Shaine Thielke <shaine.thielke@charter.com>
2016-03-19 23:20:00 -0600

    trying port 80

--- server.js
+++ server.js
@@ -92,7 +92,7 @@ http.createServer(app).listen(8000, function(){
     console.log('port 8080')
 });
 
-https.createServer(options, app).listen(443, function(){
+https.createServer(options, app).listen(80, function(){
    console.log('damnit pat'); 
 });
 
