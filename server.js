9203942b3ee7845eef4752c57508bee2e2c64577
Shaine Thielke <shaine.thielke@charter.com>
2016-03-23 19:02:29 -0600

    daskj

--- server.js
+++ server.js
@@ -15,6 +15,24 @@ var cookieParser = require('cookie-parser');
 var flash = require('connect-flash');
 var passport = require('passport');
 var passportConfig = require('./config/passport.js');
+// Set your secret key: remember to change this to your live secret key in production
+// See your keys here https://dashboard.stripe.com/account/apikeys
+var stripe = require("stripe")("sk_test_gAH4qjl2YIK1yAN5zBywEN67");
+
+// (Assuming you're using express - expressjs.com)
+// Get the credit card details submitted by the form
+var stripeToken = request.body.stripeToken;
+
+var charge = stripe.charges.create({
+    amount: 1000, // amount in cents, again
+    currency: "usd",
+    source: stripeToken,
+    description: "Example charge"
+}, function(err, charge) {
+    if (err && err.type === 'StripeCardError') {
+        // The card has been declined
+    }
+});
 
 
 
