1efb002cdc57b03f657c80d4f37e362f2d2b8a59
Shaine Thielke <shaine.thielke@charter.com>
2016-04-09 13:17:59 -0600

    dfalh

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,6 +7,8 @@ var registryControllers = angular.module('registryControllers', []);
 
 registryControllers.controller('registryCtrl', ['$scope', '$rootScope', '$http',
   function($scope, $rootScope, $http) {
+
+      Stripe.setPublishableKey('pk_test_1kGfCqltwJC8xEyFDaPLGxn3');
       
       $scope.showCC = false;
 
