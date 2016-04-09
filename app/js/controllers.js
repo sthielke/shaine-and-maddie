83b7b9c8c5de061e0ef0df9854ea72ef3ce6918d
Shaine Thielke <shaine.thielke@charter.com>
2016-04-09 13:13:43 -0600

    fdsakjh

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,7 +5,7 @@
 
 var registryControllers = angular.module('registryControllers', []);
 
-registryControllers.controller('registryCtrl', ['$scope', '$rootScope', '$http',
+registryControllers.controller('registryCtrl', ['$scope', '$rootScope', '$http', 'angularPayments',
   function($scope, $rootScope, $http) {
       
       $scope.showCC = false;
