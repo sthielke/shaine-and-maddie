677e35ecf9d1af6bd57e7ec4e8d13d77c05080a6
Shaine Thielke <shaine.thielke@charter.com>
2016-04-09 13:48:26 -0600

    fasdkjh

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,7 +7,7 @@
 var registryControllers = angular.module('registryControllers', []);
 
 
-registryControllers.controller('registryCtrl', ['$scope', '$rootScope', '$http', 'angular-payments',
+registryControllers.controller('registryCtrl', ['$scope', '$rootScope', '$http', 'angularPayments',
   function($scope, $rootScope, $http) {
 
       
